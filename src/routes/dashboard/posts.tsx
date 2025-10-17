d206303b7ff62764212d75ca218febfc5d5dd597
lalatina11 <rahmadancandra@gmail.com>
2025-10-17 19:59:03 +0700

    chore: new /dashboard/posts page

--- src/routes/dashboard/posts.tsx
+++ src/routes/dashboard/posts.tsx
@@ -1,21 +1,49 @@
+import CreatePostForm from "@/components/forms/CreatePostForm";
 import SignOutButton from "@/components/SignOutButton";
-import { getSession } from "@/lib/server/loader";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+import { getPostsWithUser, getSession } from "@/lib/server/loader";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/dashboard/posts")({
+  ssr: true,
   component: RouteComponent,
   loader: async () => {
     const session = await getSession();
-    return { session };
+    const posts = await getPostsWithUser();
+    return { session, posts };
   },
 });
 
 function RouteComponent() {
-  const { session } = Route.useLoaderData();
+  const { session, posts } = Route.useLoaderData();
+
   return (
-    <div>
-      <div>hello {session?.user.name}</div>
-      <SignOutButton />
+    <div className="flex flex-col gap-3">
+      <div>
+        <div>hello {session?.user.name}</div>
+        <SignOutButton />
+      </div>
+      <CreatePostForm />
+      {posts.length ? (
+        <div className="grid grid-cols-3 p-4 gap-3">
+          {posts.map((post) => (
+            <Card key={post.id}>
+              <CardContent>
+                <CardTitle>{post.title}</CardTitle>
+                <CardDescription>{post.description}</CardDescription>
+                <CardDescription>Author: {post.user?.name}</CardDescription>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      ) : (
+        <div>no posts available</div>
+      )}
     </div>
   );
 }
